7e931946119c3c0415e4a0414b8de16777911862
lawc021 <lawc021@163.com>
2021-06-06 20:37:19 +0800

    1

---
+++ ideaforgit/src/java/test/Test.java
@@ -0,0 +1,13 @@
+package java.test;
+
+/**
+ * 罗灿
+ * 2021/6/6
+ * 20:34
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
